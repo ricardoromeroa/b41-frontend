49fe6e4689b53d38101e3787f326da8eb8844edb
Ricardor <ricardoromeroavila@gmail.com>
2020-06-12 18:59:06 -0700

    reactstrap y preparar frontend app

---
+++ b41-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navigation />
+      <h1>Hola Mundo!</h1>
+      </React.Fragment>
+    
+  );
+}
+
+export default App;
